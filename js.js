dc55d4fbd1350d5024ce0632327e4cb1805a1e39
AmrAHegazy <61466506+AmrAHegazy@users.noreply.github.com>
2022-07-04 12:34:02 +0200

    js

---
+++ js.js
@@ -0,0 +1,51 @@
+var styles = `
+.button {
+  margin-top: 20px;
+  line-height: 60px;
+  font-weight: bold;
+  padding: 0 40px;
+  background: salmon;
+  border: none;
+  display:block;
+}
+.button:hover {
+  background: lightsalmon;
+}
+
+`
+
+
+//add-styles
+var styleSheet = document.createElement("style")
+styleSheet.innerText = styles
+document.head.appendChild(styleSheet);
+
+
+// 1. Create the button
+var button = document.createElement("button");
+button.innerHTML = "Checkout";
+button.classList.add("button");
+// 2. Append somewhere
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(button);
+
+// 3. Add iFrame div
+
+var dv = document.createElement("div");
+dv.setAttribute('id','iframe-container');
+document.body.appendChild(dv);
+
+// 4. Add event handler
+button.addEventListener ("click", function() {
+// 5.create iframe 	
+
+if(!document.getElementById('ifrm')){
+	var ifrm = document.createElement('iframe');
+    ifrm.setAttribute('id', 'ifrm'); // assign an id
+	ifrm.style.width = "640px";
+    ifrm.style.height = "480px";
+    dv.appendChild(ifrm);
+    ifrm.setAttribute('src', 'https://www.youtube.com/embed/ro_Vwk_LTHc');
+  }
+});
+
